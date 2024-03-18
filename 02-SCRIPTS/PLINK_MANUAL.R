e01660d98a91a1a0c9aed0a514632cb4e4528482
Jonathan Kitt <jonathan.kitt@inrae.fr>
2024-03-18 14:24:19 +0100

    update

--- 02-SCRIPTS/PLINK_MANUAL.R
+++ 02-SCRIPTS/PLINK_MANUAL.R
@@ -7,3 +7,9 @@ plink <- "C:/Program Files/genomics_software/PLINK.1.90/plink.exe"
 # GENERAL USAGE -----------------------------------------------------------
 
 system2(plink, "--dummy 2 2 --freq --make-bed --out 01-DATA-RAW/toy_data")
+
+# STANDARD INPUT ----------------------------------------------------------
+
+system2(plink, "--bfile 01-DATA-RAW/toy")
+
+ped <- data.table::fread("01-DATA-RAW/toy.ped", header = F)
